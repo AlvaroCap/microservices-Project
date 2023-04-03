5d3b8eebc0c0f7dd829449f1d5e16d574b8a6afb
Valverde Camaño <mario.valverde-camano@capgemini.com>
2023-04-03 13:31:01 +0200

    Cmbio

---
+++ Spring-Microservice-Event/src/main/java/com/spring/project/service/EventService.java
@@ -0,0 +1,22 @@
+package com.spring.project.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.project.model.Event;
+import com.spring.project.repository.EventRepository;
+
+@Service
+public class EventService {
+	
+	@Autowired
+	private EventRepository eventRepository;
+	
+	public List<Event> findAll(){
+		
+		return eventRepository.findAll();
+	}
+
+}
