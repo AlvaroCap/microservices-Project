9fae016ebcf61c131754e4dfa5484d35ceefe6d3
Espina Megia <alberto.espina-megia@capgemini.com>
2023-04-04 12:45:45 +0200

    Cambios en el controlador

--- Spring-Microservice-Event/src/main/java/com/spring/project/controller/EventController.java
+++ Spring-Microservice-Event/src/main/java/com/spring/project/controller/EventController.java
@@ -1,13 +1,24 @@
 package com.spring.project.controller;
 
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.project.dto.EventDTO;
+import com.spring.project.service.EventService;
 
 @Controller
 @RequestMapping("/")
 public class EventController {
 	
+	@Autowired
+	private EventService eventService;
+	
 	@GetMapping("/events")
-	public void 
+	public Set<EventDTO> findAll(){
+		return 
+	}
 }
