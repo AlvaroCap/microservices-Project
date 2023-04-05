ebc5b6f8413d25ae59a0d87da3dc5f8c8d406b18
AlvaroCap <alvaro.ruiz-navarro@capgemini.com>
2023-04-05 13:35:44 +0200

    Añadida documentacion Swagger al microservicio Buy

--- Spring-Microservice-Buy/src/main/java/com/spring/project/controller/BuyController.java
+++ Spring-Microservice-Buy/src/main/java/com/spring/project/controller/BuyController.java
@@ -8,7 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.project.dto.BuyDTO;
 import com.spring.project.dto.ErrorDTO;
+import com.spring.project.model.Buy;
 import com.spring.project.service.BuyService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @RestController
 @RequestMapping("/buy")
@@ -17,6 +25,14 @@ public class BuyController {
 	@Autowired
 	private BuyService buyService;
 
+	@Operation(summary = "Envio de datos para validar", description = "Manda informacion a un Microservicio Validador", tags = {
+			"event" })
+
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Envio de datos realizados", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Buy.class)) }),
+			@ApiResponse(responseCode = "400", description = "No válido", content = @Content),
+			@ApiResponse(responseCode = "404", description = "Ruta no encontrada para continuar con la validacion. ", content = @Content) })
 	@PostMapping
 	public String validateBuy(@RequestBody BuyDTO buyDTO) {
 		ErrorDTO errorDTO = buyService.consumingApi(buyDTO);
