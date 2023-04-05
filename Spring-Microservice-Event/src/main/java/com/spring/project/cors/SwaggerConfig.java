58fd85e49d4a58abc1740280e17f6a3bc92c02e3
Valverde Camaño <mario.valverde-camano@capgemini.com>
2023-04-05 08:21:29 +0200

    Cambio en swagger y controlador de event

--- Spring-Microservice-Event/src/main/java/com/spring/project/cors/SwaggerConfig.java
+++ Spring-Microservice-Event/src/main/java/com/spring/project/cors/SwaggerConfig.java
@@ -1,13 +1,11 @@
 package com.spring.project.cors;
 
-import java.util.Collections;
-import io.swagger.v3.oas.models.info.Contact;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
