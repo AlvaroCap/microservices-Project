ae94420fb1970bdb4f96a40993016c5cb3091246
Espina Megia <alberto.espina-megia@capgemini.com>
2023-04-05 12:46:29 +0200

    Microservicio Buy y control de errores

--- Spring-Microservice-Buy/src/main/java/com/spring/project/SpringMicroserviceBuyApplication.java
+++ Spring-Microservice-Buy/src/main/java/com/spring/project/SpringMicroserviceBuyApplication.java
@@ -3,9 +3,11 @@ package com.spring.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class SpringMicroserviceBuyApplication {
 
 	public static void main(String[] args) {
--- Spring-Microservice-Buy/src/main/java/com/spring/project/dto/BuyDTO.java
+++ Spring-Microservice-Buy/src/main/java/com/spring/project/dto/BuyDTO.java
@@ -3,33 +3,86 @@ package com.spring.project.dto;
 import java.io.Serializable;
 
 public class BuyDTO implements Serializable {
-    String userName;
-    String creditCard;
-    String expiredMonth;
-    String expiredYear;
+
+	String nombreTitular;
+    String numeroTarjeta;
+    String mesCaducidad;
+    String yearCaducidad;
     String cvv;
-    String issuer;
-    String concept;
-
-    public BuyDTO(String userName, String creditCard, String expiredMonth, String expiredYear, String cvv,
-            String issuer, String concept, String quantity) {
-        this.userName = userName;
-        this.creditCard = creditCard;
-        this.expiredMonth = expiredMonth;
-        this.expiredYear = expiredYear;
-        this.cvv = cvv;
-        this.issuer = issuer;
-        this.concept = concept;
-        this.quantity = quantity;
-    }
-
-    String quantity;
-
-    public String getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(String quantity) {
-        this.quantity = quantity;
-    }
+    String emisor;
+    String concepto;
+    String cantidad;
+    
+    
+	public BuyDTO() {
+		super();
+	}
+	public BuyDTO(String nombreTitular, String numeroTarjeta, String mesCaducidad, String yearCaducidad, String cvv,
+			String emisor, String concepto, String cantidad) {
+		super();
+		this.nombreTitular = nombreTitular;
+		this.numeroTarjeta = numeroTarjeta;
+		this.mesCaducidad = mesCaducidad;
+		this.yearCaducidad = yearCaducidad;
+		this.cvv = cvv;
+		this.emisor = emisor;
+		this.concepto = concepto;
+		this.cantidad = cantidad;
+	}
+	public String getNombreTitular() {
+		return nombreTitular;
+	}
+	public void setNombreTitular(String nombreTitular) {
+		this.nombreTitular = nombreTitular;
+	}
+	public String getNumeroTarjeta() {
+		return numeroTarjeta;
+	}
+	public void setNumeroTarjeta(String numeroTarjeta) {
+		this.numeroTarjeta = numeroTarjeta;
+	}
+	public String getMesCaducidad() {
+		return mesCaducidad;
+	}
+	public void setMesCaducidad(String mesCaducidad) {
+		this.mesCaducidad = mesCaducidad;
+	}
+	public String getYearCaducidad() {
+		return yearCaducidad;
+	}
+	public void setYearCaducidad(String yearCaducidad) {
+		this.yearCaducidad = yearCaducidad;
+	}
+	public String getCvv() {
+		return cvv;
+	}
+	public void setCvv(String cvv) {
+		this.cvv = cvv;
+	}
+	public String getEmisor() {
+		return emisor;
+	}
+	public void setEmisor(String emisor) {
+		this.emisor = emisor;
+	}
+	public String getConcepto() {
+		return concepto;
+	}
+	public void setConcepto(String concepto) {
+		this.concepto = concepto;
+	}
+	public String getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(String cantidad) {
+		this.cantidad = cantidad;
+	}
+	@Override
+	public String toString() {
+		return "BuyDTO [nombreTitular=" + nombreTitular + ", numeroTarjeta=" + numeroTarjeta + ", mesCaducidad="
+				+ mesCaducidad + ", yearCaducidad=" + yearCaducidad + ", cvv=" + cvv + ", emisor=" + emisor
+				+ ", concepto=" + concepto + ", cantidad=" + cantidad + "]";
+	}
+	
+    
 }
