00bcdb7ce898f0807db5948d03c4db1c4d2e0fe9
Valverde Camaño <mario.valverde-camano@capgemini.com>
2023-04-05 09:27:25 +0200

    Añadiendo controller de user

--- Spring-Microservice-User/src/main/java/com/project/spring/controller/UserController.java
+++ Spring-Microservice-User/src/main/java/com/project/spring/controller/UserController.java
@@ -1,15 +1,36 @@
 package com.project.spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.spring.dto.UserDTO;
+import com.project.spring.mapper.UserMapper;
+import com.project.spring.model.User;
 import com.project.spring.service.UserService;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UserController {
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private UserMapper mapper;
 
-    private UserService userService;
+	@GetMapping
+	public List<UserDTO> findAll() {
+		List<User> events = userService.findAll();
+		return mapper.convertToDto(events);
+	}
 
-    
+	@PostMapping("/save")
+	public UserDTO save(@RequestBody User user) {
+		return mapper.convertToDto(userService.save(user));
+	}
+
 }
--- Spring-Microservice-User/src/main/java/com/project/spring/dto/UserDTO.java
+++ Spring-Microservice-User/src/main/java/com/project/spring/dto/UserDTO.java
@@ -4,43 +4,46 @@ import java.io.Serializable;
 
 public class UserDTO implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    // Atributos
-    private String name;
-    private String lastName;
-    private String mail;
+	// Atributos
+	private String name;
+	private String lastName;
+	private String mail;
 
-    // Constructor con parametros
-    public UserDTO(String name, String lastName, String mail) {
-        this.name = name;
-        this.lastName = lastName;
-        this.mail = mail;
-    }
+	public UserDTO() {
+	}
 
-    // Getter y Setter
-    public String getName() {
-        return name;
-    }
+	// Constructor con parametros
+	public UserDTO(String name, String lastName, String mail) {
+		this.name = name;
+		this.lastName = lastName;
+		this.mail = mail;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	// Getter y Setter
+	public String getName() {
+		return name;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getMail() {
-        return mail;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
 
 }
